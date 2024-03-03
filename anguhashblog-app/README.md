e98cfb5b03440ad8a73d10726c79e8568e7e91ce
Esther White <monacodelisa@gmail.com>
2024-03-03 11:34:27 +0200

    updated build

--- anguhashblog-app/README.md
+++ anguhashblog-app/README.md
@@ -1,4 +1,4 @@
-# AngularHashnode
+# AnguHashBlog
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.0.
 
--- anguhashblog-app/dist/server/main.server.mjs
+++ anguhashblog-app/dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RID324UN.mjs";import"./chunk-IO4BWRCN.mjs";import"./chunk-UN4CAYFG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-53JHVL7X.mjs";import"./chunk-IO4BWRCN.mjs";import"./chunk-UN4CAYFG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
