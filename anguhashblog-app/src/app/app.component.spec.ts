9d896bb2a0614fc4e80609cdfceadb722c868d42
Esther White <monacodelisa@gmail.com>
2024-03-03 11:24:14 +0200

    images

--- anguhashblog-app/src/app/app.component.spec.ts
+++ anguhashblog-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-hashnode' title`, () => {
+  it(`should have the 'anguhashblog-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-hashnode');
+    expect(app.title).toEqual('anguhashblog-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-hashnode');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, anguhashblog-app');
   });
 });
