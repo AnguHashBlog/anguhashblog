1dcf9c7af61df32c42d3803d0c3d7a832c804c74
Esther White <monacodelisa@gmail.com>
2024-04-06 08:56:27 +0300

    updates easier share of contributing panels

--- anguhashblog-app/src/app/components/contributing/contributing.component.ts
+++ anguhashblog-app/src/app/components/contributing/contributing.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
-import {MatExpansionModule} from '@angular/material/expansion';
+import {MatExpansionModule, MatExpansionPanel} from '@angular/material/expansion';
 import { ViewportScroller } from '@angular/common';
 
 @Component({
@@ -11,10 +11,30 @@ import { ViewportScroller } from '@angular/common';
   templateUrl: './contributing.component.html',
   styleUrl: './contributing.component.scss'
 })
-export class ContributingComponent implements OnInit {
+export class ContributingComponent implements OnInit, AfterViewInit {
+  currentPanelId: string | null = null;
+  private route = inject(ActivatedRoute);
   private readonly scroller = inject(ViewportScroller);
+
+  @ViewChildren(MatExpansionPanel) panels!: QueryList<MatExpansionPanel>;
 
   ngOnInit(): void {
     this.scroller.scrollToPosition([0, 0]);
+  }
+
+  ngAfterViewInit(): void {
+    this.route.fragment.subscribe((fragment: string | null) => {
+      if (fragment) {
+        this.scrollAndOpenPanel(fragment);
+      }
+    });
+  }
+
+  scrollAndOpenPanel(panelId: string) {
+    this.currentPanelId = panelId;
+    this.panels.forEach(panel => {
+      panel.expanded = panelId === panel.id;
+    });
+    this.scroller.scrollToAnchor(panelId);
   }
 }
